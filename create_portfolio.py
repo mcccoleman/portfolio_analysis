964a2a16c616eba1503c404dcedc38d9e7c47aea
Michael Coleman <michael.coleman@equitymultiple.com>
2019-09-15 10:35:42 -0400

    wip

--- create_portfolio.py
+++ create_portfolio.py
@@ -1,10 +1,22 @@
+from create_positions import create_position
+from pprint import pprint
+
+
 class Portfolio(object):
     positions = []
 
 
-    def __init___(self,positions):
+    def __init__(self,positions):
         self.positions = positions
 
+    def loop_through_each_position(self):
+        for position in self.positions:
+            print(position)
+
 def create_portfolio(positions):
-    portfolio = Portfolio(positions)
-    return portfolio
+    return Portfolio(positions)
+
+
+portfolio = create_portfolio([create_position('spy',0.05,100),create_position('spy',0.05,100)])
+portfolio.loop_through_each_position()
+# pprint(vars(portfolio))
--- create_positions.py
+++ create_positions.py
@@ -4,6 +4,7 @@ import datetime as dt
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+from pprint import pprint
 
 
 class Position(object):
@@ -12,22 +13,27 @@ class Position(object):
     last_price = 0
     distribution_yield = 0
     mu = 0
+    number_of_shares = 0
 
-    def __init__(self,ticker,distribution_yield):
-
+    def __init__(self,ticker,distribution_yield,number_of_shares):
         start = dt.datetime(1990, 9, 11)
         end = dt.datetime(2019, 9, 11)
         prices = web.DataReader(ticker, 'yahoo', start, end)['Close']
         returns = prices.pct_change()
 
-        self.ticker = self
+        self.ticker = ticker
         self.std_dev = returns.std()
-        self.last_price = prices[-1] * (1 + np.random.normal(0, returns.std()))
+        self.last_price = prices[-1]
+        # revisit - pull information in automatically
         self.distribution_yield = distribution_yield
+        # approximation - revisit
         self.mu = (( prices[-1] - prices[0] ) / prices[0]) / (end - start).days
+        self.number_of_shares
+        # add tax-status of distributions at some point
         print('position created')
 
-def create_position(ticker,distribution_yield):
-    return Position(ticker,distribution_yield)
+def create_position(ticker,distribution_yield,number_of_shares):
+    return Position(ticker,distribution_yield,number_of_shares)
 
-create_position('spy',0.05)
+position = create_position('spy',0.05,100)
+pprint(vars(position))
