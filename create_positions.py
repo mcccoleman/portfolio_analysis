a04c11f9e1d2a45beeb39312f22b3d14651ffb0f
Michael Coleman <michael.coleman@equitymultiple.com>
2019-09-15 10:07:34 -0400

    wip

--- create_positions.py
+++ create_positions.py
@@ -8,27 +8,26 @@ from matplotlib import style
 
 class Position(object):
     ticker = ""
+    std_dev = 0
+    last_price = 0
     distribution_yield = 0
     mu = 0
 
-    def __init___(self,ticker,last_price,distribution_yield):
+    def __init__(self,ticker,distribution_yield):
 
         start = dt.datetime(1990, 9, 11)
         end = dt.datetime(2019, 9, 11)
         prices = web.DataReader(ticker, 'yahoo', start, end)['Close']
         returns = prices.pct_change()
-        first_price = prices[0]
-        last_price = prices[-1]
-        price_change = ( last_price - first_price ) / first_price
-        number_of_days = (end - start).days
-        mu = price_change / number_of_days
 
         self.ticker = self
         self.std_dev = returns.std()
-        self.last_price = last_price * (1 + np.random.normal(0, daily_vol))
+        self.last_price = prices[-1] * (1 + np.random.normal(0, returns.std()))
         self.distribution_yield = distribution_yield
-        self.mu = mu
+        self.mu = (( prices[-1] - prices[0] ) / prices[0]) / (end - start).days
+        print('position created')
 
-def create_position(ticker,last_price,distribution_yield):
-    position = Position(ticker,last_price,distribution_yield)
-    return position
+def create_position(ticker,distribution_yield):
+    return Position(ticker,distribution_yield)
+
+create_position('spy',0.05)
