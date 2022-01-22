c4b4d6b6d7387f1de5f9fd451b156c810b16b1d3
Coleman <colemanmc02@gmail.com>
2022-01-22 15:49:01 -0500

    add optimal portfolio and various refactors

--- efficient-frontier/analysis.py
+++ efficient-frontier/analysis.py
@@ -1,59 +1,70 @@
-import pandas as pd  
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import quandl
 import scipy.optimize as sco
 
-positions = ['AAPL','AMZN','GOOGL','XOM']
+positions = ['AAPL', 'AMZN', 'GOOGL', 'XOM']
 market_days_per_year = 252
 
+
 def portfolio_annualised_performance(weights, mean_returns, cov_matrix):
-    returns = np.sum(mean_returns*weights ) * market_days_per_year
-    std = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))) * np.sqrt(market_days_per_year)
+    returns = np.sum(mean_returns*weights) * market_days_per_year
+    std = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))
+                  ) * np.sqrt(market_days_per_year)
     return std, returns
-  
+
+
 def random_portfolios(num_portfolios, mean_returns, cov_matrix, risk_free_rate):
-    results = np.zeros((3,num_portfolios))
+    results = np.zeros((3, num_portfolios))
+
     weights_record = []
     for i in range(num_portfolios):
         weights = np.random.random(4)
         weights /= np.sum(weights)
         weights_record.append(weights)
-        portfolio_std_dev, portfolio_return = portfolio_annualised_performance(weights, mean_returns, cov_matrix)
-        results[0,i] = portfolio_std_dev
-        results[1,i] = portfolio_return
-        results[2,i] = (portfolio_return - risk_free_rate) / portfolio_std_dev
+        portfolio_std_dev, portfolio_return = portfolio_annualised_performance(
+            weights, mean_returns, cov_matrix)
+        results[0, i] = portfolio_std_dev
+        results[1, i] = portfolio_return
+        results[2, i] = (portfolio_return - risk_free_rate) / portfolio_std_dev
     return results, weights_record
 
+
 def display_simulated_ef_with_random(mean_returns, cov_matrix, num_portfolios, risk_free_rate):
-    results, weights = random_portfolios(num_portfolios,mean_returns, cov_matrix, risk_free_rate)
-    
+    results, weights = random_portfolios(
+        num_portfolios, mean_returns, cov_matrix, risk_free_rate)
+
     max_sharpe_idx = np.argmax(results[2])
-    sdp, rp = results[0,max_sharpe_idx], results[1,max_sharpe_idx]
-  
+    sdp, rp = results[0, max_sharpe_idx], results[1, max_sharpe_idx]
+
     plt.figure(figsize=(10, 7))
-    plt.scatter(results[0,:],results[1,:],c=results[2,:],cmap='YlGnBu', marker='o', s=10, alpha=0.3)
-    plt.scatter(sdp,rp,marker='*',color='r',s=500, label='Maximum Sharpe ratio')
+    plt.scatter(results[0, :], results[1, :], c=results[2, :],
+                cmap='YlGnBu', marker='o', s=10, alpha=0.3)
+    plt.scatter(sdp, rp, marker='*', color='r',
+                s=500, label='Maximum Sharpe ratio')
     plt.show()
+
 
 quandl.ApiConfig.api_key = 'pLAzMCYGMdJF92iuN648'
 data = quandl.get_table(
     'WIKI/PRICES',
-    ticker = positions,
-    qopts = { 'columns': ['date', 'ticker', 'adj_close'] },
-    date = { 'gte': '2016-1-1', 'lte': '2017-12-31' }, paginate=True)
-
+    ticker=positions,
+    qopts={'columns': ['date', 'ticker', 'adj_close']},
+    date={'gte': '2016-1-1', 'lte': '2017-12-31'}, paginate=True)
 
 
 df = data.set_index('date')
+print(df)
 table = df.pivot(columns='ticker')
+print(table)
 table.columns = [col[1] for col in table.columns]
-returns = table.pct_change()
 returns = table.pct_change()
 mean_returns = returns.mean()
 cov_matrix = returns.cov()
 num_portfolios = 25000
 risk_free_rate = 0.0178
 
-display_simulated_ef_with_random(mean_returns, cov_matrix, num_portfolios, risk_free_rate)
+display_simulated_ef_with_random(
+    mean_returns, cov_matrix, num_portfolios, risk_free_rate)
---
+++ optimal-portfolio/analysis.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+from itertools import permutations
+import matplotlib.pyplot as plt
+
+MARKET_DAYS_PER_YEAR = 252
+RISK_FREE_RATE = 0
+NUM_OF_SIMULATIONS = 100000
+
+data = pd.read_csv('position_adjusted_close.csv', header= 0, index_col=False)
+df = data.set_index('date')
+positions = list(df.columns)
+returns = df.pct_change()
+mean_returns = returns.mean()
+cov_matrix = returns.cov()
+
+def portfolio_annualised_performance(weights, mean_returns, cov_matrix):
+    returns = np.sum(mean_returns*weights) * MARKET_DAYS_PER_YEAR
+    std = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))
+                  ) * np.sqrt(MARKET_DAYS_PER_YEAR)
+    return std, returns
+
+def generate_random_portfolios(mean_returns, cov_matrix, risk_free_rate, num_portfolios):
+    results = np.zeros((3, num_portfolios))
+    weights_record = []
+    for i in range(num_portfolios):
+        weights = np.random.random(13)
+        weights /= np.sum(weights)
+        weights_record.append(weights)
+        portfolio_std_dev, portfolio_return = portfolio_annualised_performance(
+            weights, mean_returns, cov_matrix)
+        results[0, i] = portfolio_std_dev
+        results[1, i] = portfolio_return
+        results[2, i] = (portfolio_return - risk_free_rate) / portfolio_std_dev
+    return results, weights_record
+
+def get_index_of_max_sharpe_ratio(results):
+    return np.argmax(results[2])
+
+def display_simulated_ef_with_random(results):
+    max_sharpe_idx = get_index_of_max_sharpe_ratio(results)
+    plt.figure(figsize=(10, 7))
+    plt.scatter(results[0, :], results[1, :], c=results[2, :], cmap='YlGnBu', marker='o', s=10, alpha=0.3)
+    sdp, rp = results[0, max_sharpe_idx], results[1, max_sharpe_idx]
+    plt.scatter(sdp, rp, marker='*', color='r', s=500, label='Maximum Sharpe ratio')
+    plt.show()
+
+def get_optimal_weights_for_positions(results, weights):
+    max_sharpe_idx = get_index_of_max_sharpe_ratio(results)
+    optimal_weight = np.array(weights[max_sharpe_idx])
+    included_positions = np.array(positions)
+    optimal_weight_dataset = pd.DataFrame({'positions': included_positions, 'weights': optimal_weight}, columns=['positions', 'weights'])
+    return optimal_weight_dataset
+
+def run_simulations():
+    results, weights = generate_random_portfolios(mean_returns, cov_matrix, RISK_FREE_RATE, NUM_OF_SIMULATIONS)
+    display_simulated_ef_with_random(results)
+    print(get_optimal_weights_for_positions(results, weights))        
+
+
+
+run_simulations()
